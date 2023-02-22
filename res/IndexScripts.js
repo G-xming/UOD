d02f6ee5b087aca995046df98a97d55ccc27b3e2
G-xming <GitHub@emailed.cf>
2023-02-22 14:13:59 -0600

    f

--- res/IndexScripts.js
+++ res/IndexScripts.js
@@ -21,3 +21,7 @@ function EM(){
 function MC152IF(){
     document.body.innerHTML = '<iframe src="https://raw.githack.com/G-xming/MC152/main/index.html" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>'
 }
+
+function MS(){
+    document.body.innerHTML = '<iframe src="https://raw.githack.com/G-xming/MS/main/index.html" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>'
+}
