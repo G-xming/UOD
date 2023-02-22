68f1548d7d615c07009fd474c1f331eb5256f88d
G-xming <GitHub@emailed.cf>
2023-02-22 12:52:57 -0600

    ff

--- res/IndexScripts.js
+++ res/IndexScripts.js
@@ -5,3 +5,11 @@ function MC152(){
 function MC18(){
     location.href= 'https://g-xming.github.io/MC18/'
 }
+
+function MCB(){
+    location.href= 'https://g-xming.github.io/MCB/'
+}
+
+function MCC(){
+    location.href= 'https://g-xming.github.io/MCC/'
+}
