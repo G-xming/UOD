38418171447a03168c103690c3238fe6fb39e84a
G-xming <120670354+G-xming@users.noreply.github.com>
2023-03-07 11:34:22 -0600

    Update IndexScripts.js

--- res/IndexScripts.js
+++ res/IndexScripts.js
@@ -18,6 +18,10 @@ function EM(){
     location.href= 'https://g-xming.github.io/Play/MS'
 }
 
+function GS(){
+    location.href= 'https://g-xming.github.io/GS'
+}
+
 function MC152IF(){
     document.body.innerHTML = '<iframe src="https://raw.githack.com/G-xming/MC152/main/index.html" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>'
 }
